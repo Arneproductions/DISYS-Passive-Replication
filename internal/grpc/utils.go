6d4bc97f2592a3a006ce0da9279f15ccd1aaa615
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2022-01-04 09:09:11 +0100

    Remove the calling port from client ip
    Co-authored-by: amalieboegild <amalieboegild@users.noreply.github.com>

--- internal/grpc/utils.go
+++ internal/grpc/utils.go
@@ -2,13 +2,14 @@ package grpc
 
 import (
 	"context"
+	"strings"
 
 	"google.golang.org/grpc/peer"
 )
 
 func GetClientIpAddress(c context.Context) string {
 	if p, ok := peer.FromContext(c); ok {
-		return p.Addr.String()
+		return strings.Split(p.Addr.String(), ":")[0]
 	} else {
 		return ""
 	}
